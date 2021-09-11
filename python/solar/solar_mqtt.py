dcd20447c03c6092f0fb2451f7b9ed0a49092d08
Chris <farrah.chris@gmail.com>
2021-09-11 07:39:13 +0100

    Added some comments

--- python/solar/solar_mqtt.py
+++ python/solar/solar_mqtt.py
@@ -1,20 +1,24 @@
 import time
 
-import paho.mqtt.client as mqtt #import the client1
-broker_address="localhost" 
+#Import the MQTT client and connect to the MQTT broker at localhost
+import paho.mqtt.client as mqtt #import the client
+broker_address="localhost" #Change this line if the MQTT broker is running somewhere else
 client = mqtt.Client("P1") #create new instance
 client.connect(broker_address) #connect to broker
+
+#Set the MQTT topic to publish data to
 topic = "van/solar/power"
 
-
+#Import the Renogy Rover charge controller library and connect to the serial USB dongle
 from solarshed.controllers.renogy_rover import RenogyRover
-controller = RenogyRover('/dev/ttyUSB0', 1)
+controller = RenogyRover('/dev/ttyUSB0', 1) #Change this line if the USB dongle has a different address
 
+#Set the time between data captures in seconds
 SCRAPE_DELAY = 1
 
 
 while True:
-    payload = '{"power":' + str(controller.solar_power()) + '}'
+    payload = '{"power":' + str(controller.solar_power()) + '}' #Read the current solar power
     client.publish(topic,payload)
     time.sleep(SCRAPE_DELAY)
 
